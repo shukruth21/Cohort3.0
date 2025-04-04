08832681da8244ebc707889e8b48725e6e5d3193
shukruth21 <99413764+shukruth21@users.noreply.github.com>
2025-04-04 15:45:05 +0530

    Add files via upload

---
+++ week3/CommandLineInterface/index.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const { Command } = require('commander');
+const program = new Command();
+
+program
+  .name('counter')
+  .description('CLI to do file based tasks')
+  .version('0.8.0');
+
+
+  program.command('countlines')
+  .description('Count the number of lines in a file')
+  .argument('<file>', 'file to count')
+  .action((file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        const lines = data.split('\n').length;
+        console.log(`There are ${lines} lines in ${file}`);
+      }
+    });
+  });
+
+
+  program.command('count')
+  .description('Count the number of words in a file')
+  .argument('<file>', 'file to count')
+  .action((file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        let words=0
+        for (let i=0;i<data.length;i++){
+            if(data[i]=" "){//if data[i]=='\n' then line ++ also for the above count lines 
+                words++
+            }
+        }
+        console.log(`there are ${words} in this file`)
+      }
+    });
+});
+
+program.parse();
