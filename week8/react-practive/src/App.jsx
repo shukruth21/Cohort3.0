87b6b48f52b258711d2880319728149937a4a21c
shukruth21 <99413764+shukruth21@users.noreply.github.com>
2025-05-04 17:39:34 +0530

    Update App.jsx

--- week8/react-practive/src/App.jsx
+++ week8/react-practive/src/App.jsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react"
+import { Children, useEffect, useState } from "react"
 
-function App() {
+/*function App() {
   let [visibility,setVisibility] = useState(true)
   useEffect(function(){
     let visible= setInterval(function(){
@@ -35,7 +35,64 @@ function Counter() {
       <h1>{count}</h1>
     </div>
   );
-}
+}*/
 
+// Card component 
+/*function App(){
+  return (
+    <div>
+      <Card>
+        <h2>Card 1</h2>
+        <p> details of card</p>
+      </Card>
+      <Card>
+        <h2>
+          Card 2
+        </h2>
+        <p>details of card</p>
+
+      </Card>
+
+    </div>
+    
+    
+  )
+
+}
+function Card({children}){
+  return (
+    <div>
+      <div style={{
+            border: '1px solid #ccc',
+            borderRadius: '5px',
+            padding: '20px',
+            margin: '10px',
+            boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.1)',
+          }}>{children}</div>
+    </div>
+  )
+
+}*/
+
+//lists and keys
+const ItemList = ({ items }) => {
+  return (
+      <ul>
+          {items.map(item => (
+              <li key={item.id}>{item.name}</li>
+          ))}
+      </ul>
+  );
+};
+
+const App = () => {
+  const items = [
+      { id: 1, name: 'Item 1' },
+      { id: 2, name: 'Item 2' },
+      { id: 3, name: 'Item 3' },
+  ];
+
+  return <ItemList items={items} />;
+};
 export default App
 /// but in this after counter component re appears the count starts from 0 again
