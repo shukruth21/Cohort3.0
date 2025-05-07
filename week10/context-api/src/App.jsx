4edece2f18d9006f6d347575f702b18c6e6f7415
shukruth21 <99413764+shukruth21@users.noreply.github.com>
2025-05-07 12:53:24 +0530

    Add files via upload

---
+++ week10/context-api/src/App.jsx
@@ -0,0 +1,45 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const CountContext = createContext();
+
+function CountContextProvider({ children }) {
+  const [count, setCount] = useState(0);
+
+  return <CountContext.Provider value={{count, setCount}}>
+    {children}
+  </CountContext.Provider>
+}
+
+function Parent() {
+  return (
+    <CountContextProvider>
+      <Incrase />
+      <Decrease />
+      <Value />
+    </CountContextProvider>
+  );
+}
+
+function Decrease() {
+  const {count, setCount} = useContext(CountContext);
+  return <button onClick={() => setCount(count - 1)}>Decrease</button>;
+}
+
+function Incrase() {
+  const {count, setCount} = useContext(CountContext);
+  return <button onClick={() => setCount(count + 1)}>Increase</button>;
+}
+
+function Value() {
+  const {count} = useContext(CountContext);
+  return <p>Count: {count}</p>;
+}
+
+// App Component
+const App = () => {
+  return <div>
+    <Parent />
+  </div>
+};
+
+export default App;
